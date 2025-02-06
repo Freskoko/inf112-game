8d02678dac270ecab46e246486944b2435ca7440
Gedvyde Petkeviciute <gepet2395@uib.no>
2025-02-06 10:42:22 +0100

    Board skeleton w/ Ingvild

---
+++ src/main/java/inf112/firegirlwaterboy/model/Board.java
@@ -0,0 +1,12 @@
+package inf112.firegirlwaterboy.model;
+
+import inf112.grid.Grid;
+
+public class Board extends Grid<Character>{
+
+  public Board(int rows, int cols) {
+    super(rows, cols,'-' );
+    //TODO Auto-generated constructor stub
+  }
+
+}
---
+++ src/main/java/inf112/firegirlwaterboy/model/Model.java
@@ -0,0 +1,5 @@
+package inf112.firegirlwaterboy.model;
+
+public class Model {
+  
+}
