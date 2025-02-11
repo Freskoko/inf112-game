77ac8ace049cf02d334ba5a837ea465616a0d233
Gedvyde Petkeviciute <gepet2395@uib.no>
2025-02-11 17:01:20 +0100

    Make PlayerList a class with generic type parameter

--- src/main/java/inf112/firegirlwaterboy/model/Entity/PlayerList.java
+++ src/main/java/inf112/firegirlwaterboy/model/Entity/PlayerList.java
@@ -3,13 +3,13 @@ package inf112.firegirlwaterboy.model.Entity;
 import java.util.HashMap;
 import java.util.Iterator;
 
-public class PlayerList implements Iterable<Player> {
+public class  PlayerList<T extends Player> implements Iterable<T> {
 
   // Kan kun ha en spiller av hver type, hashmap fikser dette
   HashMap<PlayerType, Player> players = new HashMap<>();
 
   @Override
-  public Iterator<Player> iterator() {
+  public Iterator<T> iterator() {
     // TODO Auto-generated method stub
     throw new UnsupportedOperationException("Unimplemented method 'iterator'");
   }
