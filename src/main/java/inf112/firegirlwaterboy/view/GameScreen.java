7e22d4456f28d5a89f9245a217d7819e43f95b78
Gedvyde Petkeviciute <gepet2395@uib.no>
2025-02-12 23:47:25 +0100

    Added model logic to upkeep MVC. All direct changes to Entities are removed.

--- src/main/java/inf112/firegirlwaterboy/view/GameScreen.java
+++ src/main/java/inf112/firegirlwaterboy/view/GameScreen.java
@@ -2,21 +2,12 @@ package inf112.firegirlwaterboy.view;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
+//import com.badlogic.gdx.graphics.GL20; se kommentar i render()
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-import inf112.firegirlwaterboy.model.Entity.Player;
-import inf112.firegirlwaterboy.model.Model;
-import inf112.firegirlwaterboy.model.Entity.EntityList;
-import inf112.firegirlwaterboy.model.Entity.PlayerType;
+import inf112.firegirlwaterboy.controller.Controller;
 
 // Tilemap example : https://github.com/libgdx/libgdx/blob/master/tests/gdx-tests/src/com/badlogic/gdx/tests/superkoalio/SuperKoalio.java
 
@@ -24,8 +15,13 @@ public class GameScreen implements Screen {
     private OrthographicCamera camera;
     private OrthogonalTiledMapRenderer renderer;
     private TiledMap map;
-    private EntityList<Player> playerList;
-    private Model model; //usikker på hvordan vi skal løse dette.
+    private IViewModel model;
+    private Controller controller;
+
+    public GameScreen(IViewModel model, Controller controller) {
+        this.model = model;
+        this.controller = controller;
+    }
    
     @Override
     public void resize(int width, int height) {
@@ -38,27 +34,21 @@ public class GameScreen implements Screen {
     @Override
     public void show() {
         // Load map
-        map = new TmxMapLoader().load("src/main/resources/map.tmx");
-
+        model.init();
+        map = model.getMap();
         // Use OrthogonalTiledMapRenderer for 2D orthogonal maps. // mulig å legge til unit scale her senere
         renderer = new OrthogonalTiledMapRenderer(map);
-
         // Set up the camera
         camera = new OrthographicCamera();
         // camera.update(); resize() blir kalt etter show()
 
-
-        // dette bør kanskje gjøres i modelen?
-        playerList = new EntityList<Player>();
-        TiledMapTileLayer collisionLayer =  (TiledMapTileLayer) map.getLayers().get("Border");
-        playerList.addPlayer(PlayerType.FIREGIRL, new Player(new Sprite(new Texture("src/main/resources/figur.png")), collisionLayer));
     }
 
     @Override
     public void render(float delta) {
-        // Added background color (unsure if this is necessary)
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        // Added background color (usikker på om dette er nødvendig)
+        //Gdx.gl.glClearColor(0, 0, 0, 1);
+        //Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         ////////////////////////////////////////
 
         // Render the map
@@ -67,16 +57,21 @@ public class GameScreen implements Screen {
 
         // Oppdater spiller pos
 		float deltaTime = Gdx.graphics.getDeltaTime();
-        playerList.update(deltaTime);
+        model.update(deltaTime);
+
+        renderer.getBatch().begin();
+        model.draw(renderer.getBatch());
+        renderer.getBatch().end();
 
         // Om vi senere vil at kamera skal flytte seg etter spilleren:
-        // camera.position = PlayerList.getPlayerPositions();
-        
-        renderer.getBatch().begin();
-        playerList.draw(renderer.getBatch());
-        renderer.getBatch().end();
-    
-        
+        // camera.position = model.getPlayerPositions();
+    }
+
+    @Override
+    public void dispose() {
+        map.dispose();
+        renderer.dispose();
+        model.dispose();
     }
 
     @Override
@@ -90,17 +85,10 @@ public class GameScreen implements Screen {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'resume'");
     }
-
+    
     @Override
     public void hide() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'hide'");
-    }
-
-    @Override
-    public void dispose() {
-        map.dispose();
-        renderer.dispose();
-        playerList.dispose();
     }
 }
