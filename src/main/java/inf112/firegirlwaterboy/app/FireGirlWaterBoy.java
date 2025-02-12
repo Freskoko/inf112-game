dff814cc42ca3d6f052a339081ac94c0415d1255
Gedvyde Petkeviciute <gepet2395@uib.no>
2025-02-12 23:52:01 +0100

    FireGirlWaterBoy now upholds MVC split

--- src/main/java/inf112/firegirlwaterboy/app/FireGirlWaterBoy.java
+++ src/main/java/inf112/firegirlwaterboy/app/FireGirlWaterBoy.java
@@ -1,14 +1,26 @@
 package inf112.firegirlwaterboy.app;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 
+import inf112.firegirlwaterboy.controller.Controller;
+import inf112.firegirlwaterboy.model.Model;
 import inf112.firegirlwaterboy.view.GameScreen;
 
 public class FireGirlWaterBoy extends Game{
 
-  @Override
-  public void create() {
-    setScreen(new GameScreen());
+  private Model model;
+  private Controller controller;
+
+  public FireGirlWaterBoy(){
+    this.model = new Model(); 
+    this.controller = new Controller(model);
   }
 
+  @Override
+  public void create() { 
+
+    setScreen(new GameScreen(model, controller)); 
+    Gdx.input.setInputProcessor(controller); 
+  }
 }
