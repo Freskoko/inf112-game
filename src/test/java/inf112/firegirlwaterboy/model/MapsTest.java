f6126f32b5b9c4841dbe45630d6afa250e17530c
Gedvyde Petkeviciute <gepet2395@uib.no>
2025-02-13 09:18:16 +0100

    Initialize test for map klassen

---
+++ src/test/java/inf112/firegirlwaterboy/model/MapsTest.java
@@ -0,0 +1,16 @@
+package inf112.firegirlwaterboy.model;
+
+import org.junit.jupiter.api.Test;
+
+public class MapsTest {
+
+  @Test
+  void testGetLayer() {
+
+  }
+
+  @Test
+  void testGetMap() {
+
+  }
+}
