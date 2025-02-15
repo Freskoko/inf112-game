a74ed41ed15001aa04266d97f4a9af6ae1dcdb37
gufla5085 <gufla5085@uib.no>
2025-02-15 14:11:20 +0100

    Added Javadoc to FireGirlWaterBoy.java

--- src/main/java/inf112/firegirlwaterboy/app/FireGirlWaterBoy.java
+++ src/main/java/inf112/firegirlwaterboy/app/FireGirlWaterBoy.java
@@ -7,15 +7,24 @@ import inf112.firegirlwaterboy.controller.Controller;
 import inf112.firegirlwaterboy.model.Model;
 import inf112.firegirlwaterboy.view.GameScreen;
 
+/**
+ * FireGirlWaterBoy is a game where the player controls two characters, FireGirl and WaterBoy, to solve puzzles and complete levels.
+ * This class is the main class of the game, it initializes the model and controller, and sets the screen to the GameScreen.
+ * this class extends the Game class from libGDX.
+ */
 public class FireGirlWaterBoy extends Game{
 
   private Model model;
   private Controller controller;
 
+  /**
+   * Constructor for FireGirlWaterBoy, initializes the model and controller.
+   */
   public FireGirlWaterBoy(){
     this.model = new Model(); 
     this.controller = new Controller(model);
   }
+
 
   @Override
   public void create() { 
