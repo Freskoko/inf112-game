e74108113c4a34b494d74290cd7fccfc5671658d
henrikpopos <frishcoco@gmail.com>
2025-02-16 13:48:14 +0100

    model: Make map selection modular
    Removed hard-coded "map" value
    Now a model is loaded with model.init()
    and changing a map can be done with model.setMap()

--- src/main/java/inf112/firegirlwaterboy/model/Model.java
+++ src/main/java/inf112/firegirlwaterboy/model/Model.java
@@ -17,6 +17,7 @@ public class Model implements IControllableModel, IViewModel {
   private EntityList<PlayerType, Player> players;
   private GameState gameState;
   private Maps maps;
+  private String currentMapName  = "map";
 
   @Override
   public boolean changeVelocity(PlayerType playerType, int deltaX, int deltaY) {
@@ -32,15 +33,13 @@ public class Model implements IControllableModel, IViewModel {
 	public void update(float deltaTime) {
     for (Player player : players) {
       updatePlayer(player, deltaTime);
-      
-    
     }
 	}
 
   private void updatePlayer(Player player, float deltaTime) {
     player.update(deltaTime);
-    TiledMapTileLayer collisionLayer = maps.getLayer("map", "Border");
-    
+    TiledMapTileLayer collisionLayer = maps.getLayer(this.currentMapName, "Border");
+
 
     float oldX = player.getX(), oldY= player.getY();
 
@@ -130,13 +129,26 @@ public class Model implements IControllableModel, IViewModel {
 
   @Override
   public TiledMap getMap() {
-    return maps.getMap("map");
+    return maps.getMap(this.currentMapName);
   }
 
   @Override
   public void init() {
     maps = new Maps();
+    this.setMap(this.currentMapName);
     players = new EntityList<PlayerType, Player>();
     players.addPlayer(PlayerType.FIREGIRL, new Player());
-  }  
+  }
+
+  // for tests
+  public Model (String mapName) {
+    this.setMap(mapName);
+  }
+
+  // default
+  public Model(){};
+
+  private void setMap(String mapName) {
+    this.currentMapName = mapName;
+  }
 }
