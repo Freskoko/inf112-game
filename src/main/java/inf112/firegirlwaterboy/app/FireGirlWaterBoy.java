eb7ea460f8b6c35f9ac6fca86082d0a010c7f62b
gufla5085 <gufla5085@uib.no>
2025-02-22 11:35:03 +0100

    update FireGirlWaterBoy

--- src/main/java/inf112/firegirlwaterboy/app/FireGirlWaterBoy.java
+++ src/main/java/inf112/firegirlwaterboy/app/FireGirlWaterBoy.java
@@ -4,8 +4,10 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 
 import inf112.firegirlwaterboy.controller.Controller;
+import inf112.firegirlwaterboy.model.GameState;
 import inf112.firegirlwaterboy.model.Model;
 import inf112.firegirlwaterboy.view.GameScreen;
+import inf112.firegirlwaterboy.view.WelcomeScreen;
 
 public class FireGirlWaterBoy extends Game{
 
@@ -19,8 +21,8 @@ public class FireGirlWaterBoy extends Game{
 
   @Override
   public void create() { 
-
-    setScreen(new GameScreen(model, controller)); 
+    model.setGameState(GameState.WELCOME);
+    setScreen(new WelcomeScreen(model, controller)); 
     Gdx.input.setInputProcessor(controller); 
   }
 }
