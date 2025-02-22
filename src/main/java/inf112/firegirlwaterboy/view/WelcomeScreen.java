db5fd1c847117371b0a767a8ef1013f17c6f9f51
gufla5085 <gufla5085@uib.no>
2025-02-22 11:33:03 +0100

    implementing WelcomeScreen

--- src/main/java/inf112/firegirlwaterboy/view/WelcomeScreen.java
+++ src/main/java/inf112/firegirlwaterboy/view/WelcomeScreen.java
@@ -1,42 +1,67 @@
 package inf112.firegirlwaterboy.view;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+import inf112.firegirlwaterboy.controller.Controller;
+import inf112.firegirlwaterboy.model.GameState;
+import inf112.firegirlwaterboy.controller.IControllableModel;
 
 public class WelcomeScreen implements Screen {
 
-  @Override
-  public void show() {
-    // TODO Auto-generated method stub
-  }
+    private SpriteBatch batch;
+    private BitmapFont font;
+    private IControllableModel model;
+    private Controller controller;
 
-  @Override
-  public void render(float delta) {
-    // TODO Auto-generated method stub
-  }
+    public WelcomeScreen(IControllableModel model, Controller controller) {
+        this.model = model;
+        this.controller = controller;
+    }
 
-  @Override
-  public void resize(int width, int height) {
-    // TODO Auto-generated method stub
-  }
+    @Override
+    public void show() {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        font.setColor(Color.WHITE);
+        Gdx.input.setInputProcessor(controller);
+    }
 
-  @Override
-  public void pause() {
-    // TODO Auto-generated method stub
-  }
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(Color.BLACK);
 
-  @Override
-  public void resume() {
-    // TODO Auto-generated method stub
-  }
+        batch.begin();
+        font.draw(batch, "WELCOME TO FIREGIRL & WATERBOY", 100, 300);
+        font.draw(batch, "Press 'P' to Start", 150, 250);
+        batch.end();
 
-  @Override
-  public void hide() {
-    // TODO Auto-generated method stub
-  }
+        // Check for key press to start game
+        if (Gdx.input.isKeyJustPressed(Input.Keys.P)) {
+            model.setGameState(GameState.ACTIVE_GAME);
+        }
+    }
 
-  @Override
-  public void dispose() {
-    // TODO Auto-generated method stub
-  }
-    
+    @Override
+    public void resize(int width, int height) {}
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {}
+
+    @Override
+    public void hide() {}
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
 }
