ca54a0f5ae358afb938dcde81ad0d629fac47f53
Gedvyde Petkeviciute <gedvyde.petkeviciute@student.uib.no>
2025-02-26 09:32:32 +0100

    Removed unused methods, added texture Region. Created body, and added body movement on floor. (+ wall collision)

--- src/main/java/inf112/firegirlwaterboy/model/Entity/Player.java
+++ src/main/java/inf112/firegirlwaterboy/model/Entity/Player.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
@@ -11,43 +12,60 @@ import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
+import inf112.firegirlwaterboy.model.Maps;
 import inf112.firegirlwaterboy.model.Model;
 
 // Må denne være public?
 
 /**
  * Player class represents a player character in the game.
- * The player is a sprite that can move, has velocity and is affected by gravity.
+ * The player is a sprite that can move, has velocity and is affected by
+ * gravity.
  */
 public class Player extends Sprite implements IEntity {
 
-  // Endring i x og y retning
-  private Vector2 velocity;
   private float speed = 60 * 2, gravity = 60 * 1.8f;
   private World world;
   private Body body;
   private boolean onGround;
-
+  private boolean touchingWall;
   private PlayerType playerType;
 
   /**
    * Constructs a player with a given sprite and initial position.
+   * 
    * @param world The world representing the player
-   * @param pos The initial position of the player
+   * @param pos   The initial position of the player
    */
   public Player(World world, Vector2 pos, PlayerType playerType) {
-    super();
+    super(getTextureForType(playerType));
     this.world = world;
     this.playerType = playerType;
     this.onGround = false;
-    createBody(pos.x, pos.y);
+    this.touchingWall = false;
+    createBody(pos);
     setPosition(pos.x, pos.y);
   }
 
+  private static TextureRegion getTextureForType(PlayerType type) {
+    switch (type) {
+      case FIREGIRL:
+        return new TextureRegion(new Texture(Gdx.files.internal("figur.png")));
+      case WATERBOY:
+        return new TextureRegion(new Texture(Gdx.files.internal("textures/waterboy.png")));
+      default:
+        return new TextureRegion(new Texture(Gdx.files.internal("textures/default.png")));
+
+    }
+  }
 
   @Override
   public Texture getTexture() {
     return super.getTexture();
+  }
+
+  public Body getBody() {
+    return body;
   }
 
   @Override
@@ -56,83 +74,82 @@ public class Player extends Sprite implements IEntity {
     super.draw(batch);
   }
 
-  // Maybe this should be a package private method??
-  @Override
-  public void update(float deltaTime) {
-    setPosition(body.getPosition().x * Model.PPM, body.getPosition().y * Model.PPM);
-  }
-
-  /**
-   * Sets the velocity of the player.
-   * @param deltaX The velocity in x direction
-   * @param deltaY The velocity in y direction
-   */
-  public void setVelosity(int deltaX, int deltaY) {
-    velocity.set(deltaX * speed, deltaY * speed);
-  }
-
-  /**
-   * Sets the velocity of the player.
-   * @param velocity The new velocity of the player
-   */
-  public void setVelosity(Vector2 velocity) {
-    this.velocity = velocity;
-  }
-
-  /**
-   * Gets the velocity of the player.
-   * @return The velocity of the player
-   */
-  public Vector2 getVelocity() {
-    return velocity;
-  }
-
-  // Utforske for kollisjon (Get upsi)
-  @Override
-  public void setBounds(float x, float y, float width, float height) {
-    // TODO Auto-generated method stub
-    super.setBounds(x, y, width, height);
-  }
-
-   public Body getBody() {
-    return body;
-  }
-    
-
-  private void createBody(float x, float y) {
+  public void createBody(Vector2 position) {
     BodyDef bdef = new BodyDef();
+    bdef.position.set(position.x / Maps.PPM, position.y / Maps.PPM);
     bdef.type = BodyDef.BodyType.DynamicBody;
-    bdef.position.set(x / Model.PPM, y / Model.PPM);
 
-    body = world.createBody(bdef);
+    this.body = world.createBody(bdef);
 
-    PolygonShape shape = new PolygonShape();
-    shape.setAsBox(16 / Model.PPM, 16 / Model.PPM);
+
+    // Main player body (rectangle shape)
+    PolygonShape bodyShape = new PolygonShape();
+    bodyShape.setAsBox(16 / Maps.PPM, 32 / Maps.PPM); // Adjust width & height based on player sprite
 
     FixtureDef fdef = new FixtureDef();
-    fdef.shape = shape;
-    fdef.density = 1f;
-    fdef.friction = 0.6f;
-    fdef.restitution = 0.1f; // Small bounce
+    fdef.shape = bodyShape;
+    fdef.density = 1.0f;
+    fdef.friction = 0.5f;
+    fdef.restitution = 0.0f;
+    this.body.createFixture(fdef).setUserData("PLAYER");
+    body.setLinearDamping(0f);
+    bodyShape.dispose();
 
-    body.createFixture(fdef).setUserData(this);
-    shape.dispose();
+    // Foot sensor (used for detecting ground contact)
+    PolygonShape footShape = new PolygonShape();
+    footShape.setAsBox(12 / Maps.PPM, 4 / Maps.PPM, new Vector2(0, -34 / Maps.PPM), 0); // Positioned at the bottom
+
+    FixtureDef footFdef = new FixtureDef();
+    footFdef.shape = footShape;
+    footFdef.isSensor = true; // Sensor means it detects but does not physically collide
+    this.body.createFixture(footFdef).setUserData("FOOT_SENSOR");
+
+    footShape.dispose();
   }
 
-  public PlayerType getPlayerType() {
-    return this.playerType;
+  @Override
+  public void update(float deltaTime) {
+    if (onGround && body.getLinearVelocity().y < 0) {
+      body.setLinearVelocity(body.getLinearVelocity().x, 0);
+  }
+  }
+
+  /**
+   * Sets the velocity of the player.
+   * 
+   * @param dir The velocity in x direction
+   */
+  public void move(String dir) {
+    if (!touchingWall) {
+      if (dir.equals("left")){
+        body.setLinearVelocity(-speed, body.getLinearVelocity().y);
+      }
+      if (dir.equals("right")) {
+        body.setLinearVelocity(speed, body.getLinearVelocity().y);
+      }
+    } else {
+      body.setLinearVelocity(0, body.getLinearVelocity().y); // Stop movement into wall
+    }
+  
   }
 
   public void jump() {
     if (onGround) {
       body.applyLinearImpulse(new Vector2(0, 5f), body.getWorldCenter(), true);
-      onGround = false; 
+      onGround = false;
     }
   }
-  
 
   public void setOnGround(boolean onGround) {
     this.onGround = onGround;
+    // if (onGround) {
+    //   body.setLinearVelocity(body.getLinearVelocity().x, 0); // Stop downward movement
+    // }
+    System.out.println("Player on ground: " + onGround);
+  }
+
+  public void setTouchingWall(boolean touchingWall) {
+    this.touchingWall = touchingWall;
   }
 
 }
