5004f17af93cd493e2011c017b5816ffc6a99577
henrik <henellefsbro@protonmail.com>
2025-03-07 08:52:24 +0100

    controller: Define MovementType enum

---
+++ src/main/java/inf112/firegirlwaterboy/controller/MovementType.java
@@ -0,0 +1,11 @@
+package inf112.firegirlwaterboy.controller;
+
+/**
+ * Enum for the different movement states in the game.
+ */
+public enum MovementType {
+  JUMP(),
+  LEFT(),
+  RIGHT(),
+  STOP(),
+}
