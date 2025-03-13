ae2e609ea18a511ebe42a5d50f93e9c858b73d08
Gedvyde Petkeviciute <gedvyde.petkeviciute@student.uib.no>
2025-03-13 09:48:53 +0100

    Updated contactListener to respond to contact between player and coin.
    Updated contactListener to only update status of Player who is in contact. Possible for further improvement to remove loop, and retrieve player from Fixture body directly.

--- src/main/java/inf112/firegirlwaterboy/model/MyContactListener.java
+++ src/main/java/inf112/firegirlwaterboy/model/MyContactListener.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
 
+import inf112.firegirlwaterboy.model.entity.Collectable;
 import inf112.firegirlwaterboy.model.entity.EntityList;
 import inf112.firegirlwaterboy.model.entity.Player;
 import inf112.firegirlwaterboy.model.entity.PlayerType;
@@ -32,28 +33,62 @@ public class MyContactListener implements ContactListener {
     Fixture a = contact.getFixtureA();
     Fixture b = contact.getFixtureB();
     for (Player player : players) {
+
+      if (!isPlayer(a, b, player)) {
+        continue;
+      }
       // isPlayerFootSensor does not work as intended, might not need it anyway
-      /*if (isPlayerFootSensor(a, b, player)) {
-        System.out.println("it is foot");
-        if (isHorizontal(a, b)) {
-          System.out.println("cs: " + contactStatus);
-          player.setOnGround(contactStatus);
-        }
-      }*/
+      /*
+       * if (isPlayerFootSensor(a, b, player)) {
+       * System.out.println("it is foot");
+       * if (isHorizontal(a, b)) {
+       * System.out.println("cs: " + contactStatus);
+       * player.setOnGround(contactStatus);
+       * }
+       * }
+       */
       if (isHorizontal(a, b)) {
         player.setOnGround(contactStatus);
-      } 
-      else if (isVertical(a, b)) {
+      } else if (isVertical(a, b)) {
         player.setTouchingWall(contactStatus);
+      } else if (isCollectable(a, b, player)) {
+        System.out.println("collectable");
       }
     }
   }
 
-  /*private boolean isPlayerFootSensor(Fixture a, Fixture b, Player player) {
-    boolean footA = "FOOT_SENSOR".equals(a.getUserData()) && a.getBody() == player.getBody();
-    boolean footB = "FOOT_SENSOR".equals(b.getUserData()) && b.getBody() == player.getBody();
-    return (footA || footB) && (isHorizontal(b, a) || isHorizontal(a, b));
-  }*/
+  /*
+   * private boolean isPlayerFootSensor(Fixture a, Fixture b, Player player) {
+   * boolean footA = "FOOT_SENSOR".equals(a.getUserData()) && a.getBody() ==
+   * player.getBody();
+   * boolean footB = "FOOT_SENSOR".equals(b.getUserData()) && b.getBody() ==
+   * player.getBody();
+   * return (footA || footB) && (isHorizontal(b, a) || isHorizontal(a, b));
+   * }
+   */
+
+  private boolean isPlayer(Fixture a, Fixture b, Player player) {
+    return (a.getBody() == player.getBody() || b.getBody() == player.getBody());
+  }
+
+  private boolean isCollectable(Fixture a, Fixture b, Player player) {
+    if (a.getUserData() instanceof Collectable collectable) {
+      return canCollect(player, collectable);
+    }
+    if (b.getUserData() instanceof Collectable collectable) {
+      return canCollect(player, collectable);
+    }
+    return false;
+  }
+
+  private boolean canCollect(Player player, Collectable collectable) {
+    if (player.getPlayerType() == collectable.getRequiredPlayer()) {
+      player.collect();
+      collectable.collect();
+      return true;
+    }
+    return false;
+  }
 
   private boolean isVertical(Fixture a, Fixture b) {
     return "Vertical".equals(a.getUserData()) || "Vertical".equals(b.getUserData());
