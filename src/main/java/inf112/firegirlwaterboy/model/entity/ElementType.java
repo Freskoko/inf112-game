9d330db673e0fefb92da3539ace9327e2c39c2be
Ingvild Hope <inhop0904@uib.no>
2025-03-20 13:05:33 +0100

    Expanded enum for easy access to textures for elements

--- src/main/java/inf112/firegirlwaterboy/model/entity/ElementType.java
+++ src/main/java/inf112/firegirlwaterboy/model/entity/ElementType.java
@@ -4,5 +4,17 @@ package inf112.firegirlwaterboy.model.entity;
  * Enum for the two different element types in the game.
  */
 public enum ElementType {
-  LAVA, WATER
+  LAVA("lava.png"),
+  WATER("water.png");
+
+  private final String texturePath;
+
+  ElementType(String texturePath) {
+    this.texturePath = texturePath;
+  }
+
+  public String getTexturePath() {
+    return texturePath;
+  }
+
 }
