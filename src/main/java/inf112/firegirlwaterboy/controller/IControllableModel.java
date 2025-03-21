13f09afe8cde8a5b976256eccc940fd36aa4ebf0
Gedvyde Petkeviciute <gedvyde.petkeviciute@student.uib.no>
2025-03-21 11:43:12 +0100

    IController: removed methods: contains Player and getPlayerAsString

--- src/main/java/inf112/firegirlwaterboy/controller/IControllableModel.java
+++ src/main/java/inf112/firegirlwaterboy/controller/IControllableModel.java
@@ -5,34 +5,30 @@ import inf112.firegirlwaterboy.model.entity.PlayerType;
 
 /**
  * Interface for the controllable model.
- * This interface defines the methods that the controller can use to interact with the model.
+ * This interface defines the methods that the controller can use to interact
+ * with the model.
  */
 public interface IControllableModel {
 
-   /**
-     * @return current Game State
-     */
-    GameState getGameState();
+  /**
+   * @return current Game State
+   */
+  GameState getGameState();
 
-    /**
-     * Set gamestate of model to given gamestate
-     * 
-     * @param gameState
-     * 
-     */
-    void setGameState(GameState gameState);
+  /**
+   * Set gamestate of model to given gamestate
+   * 
+   * @param gameState
+   * 
+   */
+  void setGameState(GameState gameState);
 
+  /**
+   * @param player to move
+   * @param dirX
+   * @return return true if move was done, if not false
+   */
+  boolean changeDir(PlayerType player, MovementType dir);
 
-     /**
-     * @param player to move
-     * @param dirX
-     * @return return true if move was done, if not false
-     */
-    boolean changeDir(PlayerType player, MovementType dir);
-
-    void addPlayer(PlayerType player);
-
-    boolean containsPlayer(PlayerType playerType);
-
-    String getPlayersAsString();
+  void addPlayer(PlayerType playerType);
 }
