655c048dbc77645ca97d7044f177f7c90fd2e00a
Gedvyde Petkeviciute <gedvyde.petkeviciute@student.uib.no>
2025-03-21 11:47:54 +0100

    Model: Implementet methods: toString. Updated EntityList<PlayerType, Player> to EntitySet<Player>.  Update changeDir logic to allign with EntitySet logic. Method update is updated with collectable logic. Removed methods: getPlayerAsString()

--- src/main/java/inf112/firegirlwaterboy/model/Model.java
+++ src/main/java/inf112/firegirlwaterboy/model/Model.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.physics.box2d.World;
 
 import inf112.firegirlwaterboy.controller.IControllableModel;
 import inf112.firegirlwaterboy.controller.MovementType;
-import inf112.firegirlwaterboy.model.entity.EntityList;
+import inf112.firegirlwaterboy.model.entity.EntitySet;
 import inf112.firegirlwaterboy.model.entity.Player;
 import inf112.firegirlwaterboy.model.entity.PlayerType;
 import inf112.firegirlwaterboy.model.maps.IMaps;
@@ -20,46 +20,46 @@ import inf112.firegirlwaterboy.view.IViewModel;
  */
 public class Model implements IControllableModel, IViewModel {
 
-  private EntityList<PlayerType, Player> players;
+  private EntitySet<Player> players;
   private GameState gameState;
   private IMaps maps;
   private String currentMapName;
   private World world;
 
   public Model() {
-    this.world = new World(new Vector2(0, -9.8f), true); // Gravity
-    this.players = new EntityList<>();
-    world.setContactListener(new MyContactListener(players));
+    this.players = new EntitySet<>();
     this.currentMapName = "map";
+    this.maps = new Maps();
+    this.world = new World(new Vector2(0, -9.8f), true);
+    world.setContactListener(new MyContactListener());
   }
 
   @Override
   public void init() {
-    this.maps = new Maps();
     maps.init();
     maps.createObjectsInWorld(world, currentMapName);
-
     for (Player player : players) {
-        player.spawn(world, maps.getPlayerSpawn());
+      player.spawn(world, maps.getPlayerSpawn());
     }
   }
 
   @Override
   public boolean changeDir(PlayerType playerType, MovementType dir) {
-    if (!players.containsKey(playerType)) {
-      // stops crashes by not trying to move non-existant player
+    try {
+      Player player = players.getEntity(playerType);
+      player.move(dir);
+      return true;
+    } catch (IllegalArgumentException e) {
+      System.out.println(e.getMessage());
       return false;
     }
-    Player player = players.getPlayer(playerType);
-    player.move(dir);
-    return true;
   }
 
   @Override
-  public void update(float deltaTime) {
-    world.step(1 / 60f, 6, 2); // Usikker hva parameter gjør?
+  public void update() {
+    world.step(1 / 60f, 6, 2);
     for (Player player : players) {
-      player.update(deltaTime);
+      player.update();
     }
   }
 
@@ -78,32 +78,29 @@ public class Model implements IControllableModel, IViewModel {
     players.draw(batch);
   }
 
-  /** Cleans up allocated resources */
+  @Override
   public void dispose() {
     players.dispose();
   }
 
   @Override
   public TiledMap getMap() {
-    return maps.getMap(this.currentMapName);
+    return maps.getMap(currentMapName);
   }
+
+  @Override
   public void addPlayer(PlayerType playerType) {
     Player player1 = new Player(playerType);
-    players.addPlayer(playerType, player1);
+    players.addEntity(player1);
   }
 
   @Override
   public World getWorld() {
-    return this.world;
+    return world;
   }
 
   @Override
-  public boolean containsPlayer(PlayerType playerType) {
-    return this.players.containsKey(playerType);
-  }
-
-  @Override
-  public String getPlayersAsString() {
-    return this.players.toString();
+  public String toString() {
+    return players.toString();
   }
 }
