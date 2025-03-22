8f7cc98bbae5f87c27d577e5e79a5a20c174a528
Gedvyde Petkeviciute <gedvyde.petkeviciute@student.uib.no>
2025-03-22 22:12:37 +0100

    PlayerType links not to one ElementType saved in final field variable imuneTo.

--- src/main/java/inf112/firegirlwaterboy/model/entity/PlayerType.java
+++ src/main/java/inf112/firegirlwaterboy/model/entity/PlayerType.java
@@ -4,5 +4,17 @@ package inf112.firegirlwaterboy.model.entity;
  * Enum for the two different player types in the game.
  */
 public enum PlayerType {
-  FIREGIRL, WATERBOY
+
+  FIREGIRL(ElementType.LAVA),
+  WATERBOY(ElementType.WATER);
+
+  private final ElementType immuneTo;
+
+  PlayerType(ElementType immuneTo) {
+      this.immuneTo = immuneTo;
+  }
+
+  public ElementType getImmunity() {
+      return immuneTo;
+  }
 }
