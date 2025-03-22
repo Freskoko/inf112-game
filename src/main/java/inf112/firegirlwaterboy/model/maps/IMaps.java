7de89c71226a1aab571a7db28ff0a842ff4d1639
Gedvyde Petkeviciute <gedvyde.petkeviciute@student.uib.no>
2025-03-22 22:08:28 +0100

    IMaps: updated interface javadocs to reflect changes made to code. Removed init() method. getPlayerSpawn renamed to getSpawnPos in addition to now needing a map name parameter.

--- src/main/java/inf112/firegirlwaterboy/model/maps/IMaps.java
+++ src/main/java/inf112/firegirlwaterboy/model/maps/IMaps.java
@@ -1,6 +1,7 @@
 package inf112.firegirlwaterboy.model.maps;
 
 import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
@@ -8,14 +9,9 @@ import com.badlogic.gdx.physics.box2d.World;
 public interface IMaps {
 
   /**
-   * Initializes the all maps by loading all available Tiled maps from recources
-   * folder.
-   * Set currentMapName to "map"
-   */
-  void init();
-
-  /**
    * Retrieves a {@link TiledMap} by its name.
+   * If maps not initialized: initializes the all maps by loading all available Tiled maps from recources
+   * folder.
    *
    * @param mapName The name of the map to retrieve.
    * @return The corresponding {@link TiledMap}.
@@ -34,16 +30,18 @@ public interface IMaps {
   MapLayer getLayer(String mapName, String layerName);
 
   /**
-   * Gets the player's spawn position from the "Spawn" object layer in the current
-   * Tiled map.
+   * Gets the player's spawn position from the "Spawn" object layer in the specified
+   * map.
    * Returns a default position if the layer or objects are missing.
+   * 
+   * @param mapName The name of the map to get the spawn position from.
    *
    * @return The spawn position as a {@link Vector2}, or {@code DEFAULT_SPAWN_POS}
    *         if missing.
    * @throws NullPointerException If the "Spawn" object lacks valid X/Y
    *                              properties.
    */
-  Vector2 getPlayerSpawn();
+  Vector2 getSpawnPos(String mapName);
 
   /**
    * Creates physics objects in the given Box2D world from all object layers in
