77e264e5e10d7f67b140ae57cf4e5efbb96ccf3f
Gedvyde Petkeviciute <gedvyde.petkeviciute@student.uib.no>
2025-03-23 22:05:36 +0100

    IPlayer: Added method  interactWithElement to interface.

--- src/main/java/inf112/firegirlwaterboy/model/entity/IPlayer.java
+++ src/main/java/inf112/firegirlwaterboy/model/entity/IPlayer.java
@@ -1,5 +1,6 @@
 package inf112.firegirlwaterboy.model.entity;
 
+import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 
@@ -33,5 +34,14 @@ public interface IPlayer {
   void spawn(World world, Vector2 pos);
 
   int getCountCollected();
+
+  boolean isAlive();
+
+  /**
+   * Handles the player's interaction with an element.
+   * 
+   * @param elementType the type of element, for example LAVA or WATER
+   */
+  void interactWithElement(ElementType elementType);
 
 }
