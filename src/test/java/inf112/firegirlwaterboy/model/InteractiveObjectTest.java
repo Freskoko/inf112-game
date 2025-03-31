c67d2aaafbd57ac72a4b46440398e59d2bc1104b
henrik <henellefsbro@protonmail.com>
2025-03-31 12:39:01 +0200

    tests(InteractiveObject): init

---
+++ src/test/java/inf112/firegirlwaterboy/model/InteractiveObjectTest.java
@@ -0,0 +1,20 @@
+package inf112.firegirlwaterboy.model;
+
+import org.junit.jupiter.api.Test;
+
+public class InteractiveObjectTest {
+    @Test
+    void testGetBounds() {
+
+    }
+
+    @Test
+    void testGetValue() {
+
+    }
+
+    @Test
+    void testIsBlocked() {
+
+    }
+}
