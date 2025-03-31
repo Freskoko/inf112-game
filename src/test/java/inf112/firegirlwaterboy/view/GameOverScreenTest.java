145fbdea0ba8c6504b6a32f23bfb6a92fd05d2c4
henrik <henellefsbro@protonmail.com>
2025-03-31 12:37:41 +0200

    tests(GameOverScreen): init

---
+++ src/test/java/inf112/firegirlwaterboy/view/GameOverScreenTest.java
@@ -0,0 +1,40 @@
+package inf112.firegirlwaterboy.view;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class GameOverScreenTest {
+
+    @Test
+    @ManualTest
+    void restartGame() {
+        /*
+         * Upon dying, select "Back to Choose Maps Screen"
+         * You will be directed to a screen where you can select which map you wish to
+         * play
+         */
+        assertTrue(true);
+    }
+
+    @Test
+    @ManualTest
+    void exitScreen() {
+        /*
+         * Upon dying, exit the game by pressing the X button in the top right, to leave
+         */
+        assertTrue(true);
+    }
+
+    @Test
+    @ManualTest
+    @Disabled // TODO IMPLEMENT ME, Widgets move unsually.
+    void testResize() {
+        /*
+         * Upon dying, try to "Resize" the GameOverScreen. The widgets on screen move accordingly
+         */
+        assertTrue(true);
+    }
+
+}
