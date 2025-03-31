92f94de27325b6f0541f11586995af3634a26318
henrik <henellefsbro@protonmail.com>
2025-03-31 12:37:31 +0200

    tests(GameScreenTest): init

---
+++ src/test/java/inf112/firegirlwaterboy/view/GameScreenTest.java
@@ -0,0 +1,40 @@
+package inf112.firegirlwaterboy.view;
+
+import org.junit.jupiter.api.Test;
+
+public class GameScreenTest {
+    @Test
+    void testDispose() {
+
+    }
+
+    @Test
+    void testHide() {
+
+    }
+
+    @Test
+    void testPause() {
+
+    }
+
+    @Test
+    void testRender() {
+
+    }
+
+    @Test
+    void testResize() {
+
+    }
+
+    @Test
+    void testResume() {
+
+    }
+
+    @Test
+    void testShow() {
+
+    }
+}
