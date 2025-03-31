1fa558851019c97e9d8049863d916abe9f2203af
henrik <henellefsbro@protonmail.com>
2025-03-31 12:36:59 +0200

    tests(WelcomeScreen): init

---
+++ src/test/java/inf112/firegirlwaterboy/view/WelcomeScreenTest.java
@@ -0,0 +1,28 @@
+package inf112.firegirlwaterboy.view;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class WelcomeScreenTest {
+    @Test
+    @ManualTest
+    void getInformation() {
+        /*
+         * Upon entering the WelcomeScreen, the game's title is clearly visible
+         */
+
+        assertTrue(true);
+    }
+
+    @Test
+    @ManualTest
+    @Disabled // TODO IMPLEMENT ME, The image moves unsually.
+    void testResize() {
+        /*
+         * Resizing the WelcomeScreen causes on screen widgets move accordingly, nicely taking up place.
+         */
+        assertTrue(true);
+    }
+}
