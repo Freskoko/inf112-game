4c9831846397834ea8684d90f01fed6d90ea8111
Gedvyde Petkeviciute <gedvyde.petkeviciute@student.uib.no>
2025-03-31 20:00:52 +0200

    Changed Entity folder name in test to entity to match src

--- src/test/java/inf112/firegirlwaterboy/model/Entity/PlayerTest.java
+++ src/test/java/inf112/firegirlwaterboy/model/Entity/PlayerTest.java
@@ -1,4 +1,4 @@
-package inf112.firegirlwaterboy.model.Entity;
+package inf112.firegirlwaterboy.model.entity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
