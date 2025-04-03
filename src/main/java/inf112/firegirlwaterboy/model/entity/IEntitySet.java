00476ab3df4d7fba01876c9754debca84fae5298
Gedvyde Petkeviciute <gedvyde.petkeviciute@student.uib.no>
2025-04-03 11:38:23 +0200

    IEntitySet: updated method added to interface.

--- src/main/java/inf112/firegirlwaterboy/model/entity/IEntitySet.java
+++ src/main/java/inf112/firegirlwaterboy/model/entity/IEntitySet.java
@@ -35,4 +35,9 @@ public interface IEntitySet<E extends IEntity> {
    * @return true if entitySet contains enitity
    */
   boolean contains(E entity);
+
+  /**
+   * Updates all entities in the set. Used for updatating entity state.
+   */
+  void update();
 }
