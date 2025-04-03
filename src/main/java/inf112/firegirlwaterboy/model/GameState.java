ea51a36386accc9609e4bad93302e9ed4f0b293b
Ingvild Hope <inhop0904@uib.no>
2025-04-03 11:31:07 +0200

    Added COMPLETED_LEVEL gamestate

--- src/main/java/inf112/firegirlwaterboy/model/GameState.java
+++ src/main/java/inf112/firegirlwaterboy/model/GameState.java
@@ -4,5 +4,5 @@ package inf112.firegirlwaterboy.model;
  * Enum for the different game states in the game.
  */
 public enum GameState {
-  WELCOME(), ACTIVE_GAME(), CHOOSE_MAP(), HELP(), GAME_OVER();
+  WELCOME(), ACTIVE_GAME(), CHOOSE_MAP(), HELP(), GAME_OVER(), COMPLETED_LEVEL();
 }
