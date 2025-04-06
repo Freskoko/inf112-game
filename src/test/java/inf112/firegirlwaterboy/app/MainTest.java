d63ee5fbabdd0f1f2cd171c618b30628ea453d40
henrik <henellefsbro@protonmail.com>
2025-04-06 18:31:58 +0200

    tests(Main): init

---
+++ src/test/java/inf112/firegirlwaterboy/app/MainTest.java
@@ -0,0 +1,15 @@
+package inf112.firegirlwaterboy.app;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+    @Test
+    void testMainCompiles() {
+        assertDoesNotThrow(() -> {
+            Class<?> clazz = Class.forName("inf112.firegirlwaterboy.app.Main");
+            clazz.getMethod("main", String[].class);
+        });
+    }
+}
