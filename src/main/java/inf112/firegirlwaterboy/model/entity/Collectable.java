930f93743a3015787f7b04b4ace43c0bb9739733
Gedvyde Petkeviciute <gedvyde.petkeviciute@student.uib.no>
2025-04-08 15:41:00 +0200

    Collectable: implemented collectable type. updated collectable logic so view matches collectable status. collectables can now be collected by multiple players.

--- src/main/java/inf112/firegirlwaterboy/model/entity/Collectable.java
+++ src/main/java/inf112/firegirlwaterboy/model/entity/Collectable.java
@@ -1,5 +1,10 @@
 package inf112.firegirlwaterboy.model.entity;
 
+import java.util.Map;
+import java.util.Set;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -9,34 +14,43 @@ import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
-import inf112.firegirlwaterboy.model.maps.MapsFactory;
+import inf112.firegirlwaterboy.model.maps.MapUtils;
+import inf112.firegirlwaterboy.model.types.CollectableType;
 import inf112.firegirlwaterboy.model.types.PlayerType;
 
-public class Collectable implements IEntity<String>, ICollectable {
+public class Collectable implements IEntity<CollectableType>, ICollectable {
 
-  private PlayerType requiredPlayer;
+  private Set<PlayerType> requiredPlayer;
   private Body body;
   private World world;
   private boolean powerUp;
+  private CollectableType type;
+  private Texture texture;
+  private float x, y, width, height;
+  private boolean isCollected;
 
   public Collectable(World world, MapObject collectable) {
-    float width = MapsFactory.getWidth(collectable);
-    float height = MapsFactory.getHeight(collectable);
-    this.requiredPlayer = PlayerType.valueOf(MapsFactory.getProperty(collectable, "PlayerType"));
+    this.type = CollectableType.valueOf(MapUtils.getProperty(collectable, "type"));
+    this.powerUp = type.isPowerUp();
+    this.requiredPlayer = type.getRequiredPlayers();
+    this.texture = type.getTexture();
     this.world = world;
-    this.powerUp = Boolean.parseBoolean(MapsFactory.getProperty(collectable, "PowerUp"));
+    this.isCollected = false;
 
+    this.width = MapUtils.getWidth(collectable);
+    this.height = MapUtils.getHeight(collectable);
+    this.x = MapUtils.getX(collectable);
+    this.y = MapUtils.getY(collectable);
 
     BodyDef bdef = new BodyDef();
-    bdef.position.set(MapsFactory.getCX(collectable), MapsFactory.getCY(collectable));
+    bdef.position.set(MapUtils.getCX(collectable), MapUtils.getCY(collectable));
     bdef.type = BodyDef.BodyType.StaticBody;
     body = world.createBody(bdef);
-
     PolygonShape shape = new PolygonShape();
     shape.setAsBox(width / 2, height / 2);
+
     FixtureDef fdef = new FixtureDef();
     fdef.shape = shape;
-
     Fixture fixture = body.createFixture(fdef);
     fixture.setSensor(true);
     fixture.setUserData(this);
@@ -44,14 +58,8 @@ public class Collectable implements IEntity<String>, ICollectable {
   }
 
   @Override
-  public PlayerType getRequiredPlayer() {
+  public Set<PlayerType> getRequiredPlayer() {
     return requiredPlayer;
-  }
-
-  @Override
-  public void collect() {
-    this.world.destroyBody(body);
-    body.setUserData(null);
   }
 
   @Override
@@ -61,18 +69,20 @@ public class Collectable implements IEntity<String>, ICollectable {
 
   @Override
   public void update() {
-    
+    if (isCollected) {
+      world.destroyBody(body);
+      body.setUserData(null);
+    }
   }
 
   @Override
   public void draw(Batch batch) {
-    
+    batch.draw(texture, x, y, width, height);
   }
 
   @Override
   public void dispose() {
-    
-    
+    texture.dispose();
   }
 
   @Override
@@ -81,11 +91,19 @@ public class Collectable implements IEntity<String>, ICollectable {
   }
 
   @Override
-  public String getType() {
-    return "Collectable";
+  public CollectableType getType() {
+    return type;
   }
 
-  public boolean getPowerUp() {
+  public boolean isPowerUp() {
     return powerUp;
+  }
+
+  public boolean isCollected() {
+    return isCollected;
+  }
+
+  public void collect() {
+    this.isCollected = true;
   }
 }
