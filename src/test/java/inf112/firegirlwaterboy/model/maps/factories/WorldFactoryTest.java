04ccde32ee8b709f9ffb66ba554369e3a10fbf40
henrikpopos <frishcoco@gmail.com>
2025-04-23 23:47:40 +0200

    test(WorldFactoryTest): Fix after refactor

--- src/test/java/inf112/firegirlwaterboy/model/maps/factories/WorldFactoryTest.java
+++ src/test/java/inf112/firegirlwaterboy/model/maps/factories/WorldFactoryTest.java
@@ -1,104 +1,243 @@
 package inf112.firegirlwaterboy.model.maps.factories;
 
-import static org.mockito.Mockito.mock;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapLayers;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.maps.objects.PolygonMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
 
 import inf112.firegirlwaterboy.app.FireGirlWaterBoy;
+import inf112.firegirlwaterboy.model.entity.Element;
+import inf112.firegirlwaterboy.model.entity.Platform;
+import inf112.firegirlwaterboy.model.managers.CollectableSet;
+import inf112.firegirlwaterboy.model.managers.EntitySet;
+import inf112.firegirlwaterboy.model.maps.IMapManager;
+import inf112.firegirlwaterboy.model.maps.LayerType;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 
 public class WorldFactoryTest {
-  //private WorldFactory maps;
+    private WorldFactory worldFactory;
+    private IMapManager mapManager;
+    private IGameObjectFactory gameObjectFactory;
 
-  /*
-   * Sets up a headless version of the app, reuqired to load map/ images files
-   */
-  @BeforeAll
-  static void setUp() {
-    HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
-    new HeadlessApplication(new FireGirlWaterBoy(), config);
+    @BeforeAll
+    static void setUp() {
+        HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
+        new HeadlessApplication(new FireGirlWaterBoy(), config);
+        GL20 gl20 = mock(GL20.class);
+        Gdx.gl = gl20;
+        Gdx.gl20 = gl20;
+    }
 
-    GL20 gl20 = mock(GL20.class);
-    Gdx.gl = gl20;
-    Gdx.gl20 = gl20;
-  }
+    @BeforeEach
+    private void initTest() {
+        mapManager = mock(IMapManager.class);
+        gameObjectFactory = mock(IGameObjectFactory.class);
+        this.worldFactory = new WorldFactory(gameObjectFactory, mapManager);
+    }
 
-  @BeforeEach
-  private void initTest() {
-    //IMapManager manager = new MapManager();
-    //this.maps = new WorldFactory(new StandardGameObjectsFactory(), manager);
-  }
+    @Test
+    public void testCreateWorld() {
+        TiledMap mockMap = mock(TiledMap.class);
+        MapLayers mockLayers = mock(MapLayers.class);
+        when(mockMap.getLayers()).thenReturn(mockLayers);
 
+        Iterator<MapLayer> emptyIterator = Collections.emptyIterator();
+        when(mockLayers.iterator()).thenReturn(emptyIterator);
 
-  // meotde fra maps facotry flyttet til world factory, testene er kopiert inn hit
+        World world = worldFactory.createWorld(mockMap);
 
+        assertNotNull(world);
+        assertEquals(new Vector2(0, -9.8f), world.getGravity());
+    }
 
-  // @Test
-  // public void testEmptyBodyDoesNotCreateBodies() throws Exception {
-  //   java.lang.reflect.Method method = WorldFactory.class.getDeclaredMethod(
-  //       "createObjectsFromLayer", World.class, MapLayer.class);
-  //   method.setAccessible(true);
+    @Test
+    public void testCreatePlatforms() {
+        TiledMap mockMap = mock(TiledMap.class);
+        World mockWorld = mock(World.class);
+        MapLayer mockLayer = mock(MapLayer.class);
+        MapObject mockObject = mock(MapObject.class);
+        Platform mockPlatform = mock(Platform.class);
 
-  //   World mockWorld = mock(World.class);
-  //   MapLayer mockLayer = mock(MapLayer.class);
-  //   MapObjects mockObjects = mock(MapObjects.class);
+        when(mapManager.getLayer(mockMap, LayerType.PLATFORM)).thenReturn(mockLayer);
 
-  //   when(mockLayer.getObjects()).thenReturn(mockObjects);
-  //   when(mockObjects.iterator()).thenReturn(java.util.Collections.emptyIterator());
+        MapObjects objects = mock(MapObjects.class);
+        ArrayList<MapObject> objectList = new ArrayList<>();
+        objectList.add(mockObject);
+        when(mockLayer.getObjects()).thenReturn(objects);
+        when(objects.iterator()).thenReturn(objectList.iterator());
 
-  //   Body mockBody = mock(Body.class);
-  //   when(mockWorld.createBody(any(BodyDef.class))).thenReturn(mockBody);
+        when(gameObjectFactory.createPlatform(mockWorld, mockObject)).thenReturn(mockPlatform);
 
-  //   method.invoke(maps, mockWorld, mockLayer);
+        EntitySet<Platform> platforms = worldFactory.createPlatforms(mockWorld, mockMap);
 
-  //   verify(mockWorld, never()).createBody(any(BodyDef.class));
-  // }
+        assertNotNull(platforms);
+        verify(gameObjectFactory).createPlatform(mockWorld, mockObject);
+    }
 
-  // @Test
-  // public void testEmptyBodyDoesCreateBodies() throws Exception {
-  //   java.lang.reflect.Method method = WorldFactory.class.getDeclaredMethod(
-  //       "createObjectsFromLayer", World.class, MapLayer.class);
-  //   method.setAccessible(true);
+    @Test
+    public void testCreateElements() {
+        TiledMap mockMap = mock(TiledMap.class);
+        World mockWorld = mock(World.class);
+        MapLayer mockLayer = mock(MapLayer.class);
+        MapObject mockObject = mock(MapObject.class);
+        Element mockElement = mock(Element.class);
 
-  //   World mockWorld = mock(World.class);
-  //   MapLayer mockLayer = mock(MapLayer.class);
-  //   MapObjects mockObjects = mock(MapObjects.class);
-  //   MapObject mockObject = mock(MapObject.class);
-  //   MapProperties mockProperties = mock(MapProperties.class);
+        when(mapManager.getLayer(mockMap, LayerType.ELEMENT)).thenReturn(mockLayer);
 
-  //   Body mockBody = mock(Body.class);
-  //   Fixture mockFixture = mock(Fixture.class);
-  //   when(mockWorld.createBody(any(BodyDef.class))).thenReturn(mockBody);
-  //   when(mockBody.createFixture(any(FixtureDef.class))).thenReturn(mockFixture);
+        MapObjects objects = mock(MapObjects.class);
+        ArrayList<MapObject> objectList = new ArrayList<>();
+        objectList.add(mockObject);
+        when(mockLayer.getObjects()).thenReturn(objects);
+        when(objects.iterator()).thenReturn(objectList.iterator());
 
-  //   // setting up a layer with one object
-  //   when(mockLayer.getObjects()).thenReturn(mockObjects);
-  //   when(mockLayer.getName()).thenReturn("TestLayer"); 
-  //   ArrayList<MapObject> objectList = new ArrayList<>();
-  //   objectList.add(mockObject);
-  //   when(mockObjects.iterator()).thenReturn(objectList.iterator());
+        when(gameObjectFactory.createElement(mockWorld, mockObject)).thenReturn(mockElement);
 
-  //   // setting up properties of layer
-  //   when(mockObject.getProperties()).thenReturn(mockProperties);
-  //   when(mockProperties.get("x", Float.class)).thenReturn(100f);
-  //   when(mockProperties.get("y", Float.class)).thenReturn(200f);
-  //   when(mockProperties.get("width", Float.class)).thenReturn(50f);
-  //   when(mockProperties.get("height", Float.class)).thenReturn(30f);
+        EntitySet<Element> elements = worldFactory.createElements(mockWorld, mockMap);
 
-  //   method.invoke(maps, mockWorld, mockLayer);
+        assertNotNull(elements);
+        verify(gameObjectFactory).createElement(mockWorld, mockObject);
+    }
 
-  //   verify(mockWorld, times(1)).createBody(any(BodyDef.class));
+    @Test
+    public void testCreateCollectables() {
+        TiledMap mockMap = mock(TiledMap.class);
+        World mockWorld = mock(World.class);
+        MapLayer mockLayer = mock(MapLayer.class);
+        MapObject mockObject = mock(MapObject.class);
 
-  //   verify(mockBody, times(1)).createFixture(any(FixtureDef.class));
+        when(mapManager.getLayer(mockMap, LayerType.COLLECTABLE)).thenReturn(mockLayer);
 
-  //   verify(mockFixture).setUserData("TestLayer");
-  // }
+        MapObjects objects = mock(MapObjects.class);
+        ArrayList<MapObject> objectList = new ArrayList<>();
+        objectList.add(mockObject);
+        when(mockLayer.getObjects()).thenReturn(objects);
+        when(objects.iterator()).thenReturn(objectList.iterator());
 
- 
+        CollectableSet collectables = worldFactory.createCollectables(mockWorld, mockMap);
 
+        assertNotNull(collectables);
+        verify(gameObjectFactory).createCollectable(mockWorld, mockObject);
+    }
+
+    @Test
+    public void testCreateStaticObjects() throws Exception {
+        Method method = WorldFactory.class.getDeclaredMethod(
+                "createStaticObjects", World.class, MapLayer.class);
+        method.setAccessible(true);
+
+        World mockWorld = mock(World.class);
+        MapLayer mockLayer = mock(MapLayer.class);
+        MapObjects mockObjects = mock(MapObjects.class);
+        MapObject mockObject = mock(MapObject.class);
+        MapProperties mockProperties = mock(MapProperties.class);
+        Body mockBody = mock(Body.class);
+        Fixture mockFixture = mock(Fixture.class);
+
+        when(mockObject.getProperties()).thenReturn(mockProperties);
+        when(mockProperties.get("x", Float.class)).thenReturn(100f);
+        when(mockProperties.get("y", Float.class)).thenReturn(200f);
+        when(mockProperties.get("width", Float.class)).thenReturn(50f);
+        when(mockProperties.get("height", Float.class)).thenReturn(30f);
+
+        when(mockLayer.getName()).thenReturn(LayerType.STATIC.name());
+        when(mockLayer.getObjects()).thenReturn(mockObjects);
+        ArrayList<MapObject> objectList = new ArrayList<>();
+        objectList.add(mockObject);
+        when(mockObjects.iterator()).thenReturn(objectList.iterator());
+        when(mockWorld.createBody(any(BodyDef.class))).thenReturn(mockBody);
+        when(mockBody.createFixture(any(FixtureDef.class))).thenReturn(mockFixture);
+
+        method.invoke(worldFactory, mockWorld, mockLayer);
+
+        verify(mockWorld).createBody(any(BodyDef.class));
+        verify(mockBody).createFixture(any(FixtureDef.class));
+        verify(mockFixture).setUserData(LayerType.STATIC);
+    }
+
+    @Test
+    public void testCreatePolygonObject() throws Exception {
+        Method method = WorldFactory.class.getDeclaredMethod(
+                "createPolygonObject", World.class, PolygonMapObject.class, LayerType.class);
+        method.setAccessible(true);
+
+        World mockWorld = mock(World.class);
+        PolygonMapObject mockPolygon = mock(PolygonMapObject.class);
+        Polygon mockPolygonShape = mock(Polygon.class);
+        Body mockBody = mock(Body.class);
+        Fixture mockFixture = mock(Fixture.class);
+
+        when(mockPolygon.getPolygon()).thenReturn(mockPolygonShape);
+        float[] vertices = new float[]{0, 0, 10, 0, 10, 10, 0, 10};
+        when(mockPolygonShape.getTransformedVertices()).thenReturn(vertices);
+        when(mockWorld.createBody(any(BodyDef.class))).thenReturn(mockBody);
+        when(mockBody.createFixture(any(FixtureDef.class))).thenReturn(mockFixture);
+
+        method.invoke(worldFactory, mockWorld, mockPolygon, LayerType.STATIC);
+
+        verify(mockWorld).createBody(any(BodyDef.class));
+        verify(mockBody).createFixture(any(FixtureDef.class));
+        verify(mockFixture).setUserData(LayerType.STATIC);
+    }
+
+    @Test
+    public void testCreateFinishObjects() throws Exception {
+        Method method = WorldFactory.class.getDeclaredMethod(
+                "createFinishObjects", World.class, MapLayer.class);
+        method.setAccessible(true);
+
+        World mockWorld = mock(World.class);
+        MapLayer mockLayer = mock(MapLayer.class);
+        MapObjects mockObjects = mock(MapObjects.class);
+        MapObject mockObject = mock(MapObject.class);
+        Body mockBody = mock(Body.class);
+        Fixture mockFixture = mock(Fixture.class);
+        MapProperties mockProperties = mock(MapProperties.class);
+
+        when(mockLayer.getName()).thenReturn(LayerType.FINISH.name());
+        when(mockLayer.getObjects()).thenReturn(mockObjects);
+        ArrayList<MapObject> objectList = new ArrayList<>();
+        objectList.add(mockObject);
+        when(mockObjects.iterator()).thenReturn(objectList.iterator());
+        when(mockObject.getProperties()).thenReturn(mockProperties);
+        when(mockProperties.get("x", Float.class)).thenReturn(100f);
+        when(mockProperties.get("y", Float.class)).thenReturn(200f);
+        when(mockProperties.get("width", Float.class)).thenReturn(50f);
+        when(mockProperties.get("height", Float.class)).thenReturn(30f);
+        when(mockWorld.createBody(any(BodyDef.class))).thenReturn(mockBody);
+        when(mockBody.createFixture(any(FixtureDef.class))).thenReturn(mockFixture);
+
+        method.invoke(worldFactory, mockWorld, mockLayer);
+
+        verify(mockWorld).createBody(any(BodyDef.class));
+        verify(mockBody).createFixture(any(FixtureDef.class));
+        verify(mockFixture).setUserData(LayerType.FINISH);
+        verify(mockFixture).setSensor(true);
+    }
 }
