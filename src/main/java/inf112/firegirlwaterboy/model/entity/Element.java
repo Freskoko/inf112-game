cd442226f7b2c34391d9a10956233d1832e9a55b
Ingvild Hope <inhop0904@uib.no>
2025-04-24 08:33:52 +0200

    Added animation of elements and moved creation of body to its own method in IEntity

--- src/main/java/inf112/firegirlwaterboy/model/entity/Element.java
+++ src/main/java/inf112/firegirlwaterboy/model/entity/Element.java
@@ -2,14 +2,18 @@ package inf112.firegirlwaterboy.model.entity;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 
 import inf112.firegirlwaterboy.model.maps.LayerType;
 import inf112.firegirlwaterboy.model.maps.MapUtils;
@@ -20,24 +24,33 @@ import inf112.firegirlwaterboy.model.types.ElementType;
  * Elements are static objects that can be interacted with by players.
  */
 public class Element implements IEntity<ElementType> {
-  private Texture texture;
+  private Animation<TextureRegion> animation;
+  private float stateTime = 0f;
   private ElementType type;
   private Body body;
   private float x, y, width, height;
+  private Vector2 pos;
 
   public Element(World world, MapObject element) {
     this.type = ElementType.valueOf(MapUtils.getProperty(element, "type"));
-    this.texture = new Texture(Gdx.files.internal(type.getTexturePath()));
+    this.animation = createAnimation(type);
 
     this.width = MapUtils.getWidth(element);
     this.height = MapUtils.getHeight(element);
     this.x = MapUtils.getX(element);
     this.y = MapUtils.getY(element);
+    this.pos = new Vector2(MapUtils.getCX(element), MapUtils.getCY(element));
 
+    createBody(world, pos);
+  }
+  
+  @Override
+  public void createBody(World world, Vector2 pos) {
     BodyDef bdef = new BodyDef();
-    bdef.position.set(MapUtils.getCX(element), MapUtils.getCY(element));
+    bdef.position.set(pos);
     bdef.type = BodyDef.BodyType.StaticBody;
-    body = world.createBody(bdef);
+    this.body = world.createBody(bdef);
+
     PolygonShape shape = new PolygonShape();
     shape.setAsBox(width / 2, height / 2);
 
@@ -45,24 +58,30 @@ public class Element implements IEntity<ElementType> {
     fdef.shape = shape;
     fdef.filter.categoryBits = LayerType.ELEMENT.getBit();
     fdef.filter.maskBits = LayerType.PLAYER.getBit();
+
     Fixture fixture = body.createFixture(fdef);
     fixture.setSensor(true);
     fixture.setUserData(this);
+
     shape.dispose();
   }
 
   @Override
   public void update() {
+    stateTime += Gdx.graphics.getDeltaTime();
   }
 
   @Override
   public void draw(Batch batch) {
-    batch.draw(texture, x, y, width, height);
+    TextureRegion currentFrame = animation.getKeyFrame(stateTime, true);
+    batch.draw(currentFrame, x, y, width, height);
   }
 
   @Override
   public void dispose() {
-    texture.dispose();
+    for (TextureRegion frame : animation.getKeyFrames()) {
+      frame.getTexture().dispose();
+    }
   }
 
   @Override
@@ -70,4 +89,20 @@ public class Element implements IEntity<ElementType> {
     return type;
   }
 
+  /**
+   * Creates an animation for the element based on its type.
+   * 
+   * @param type The type of the element.
+   * @return The animation for the element.
+   */
+  private Animation<TextureRegion> createAnimation(ElementType type) {
+    String[] paths = type.getTexturePaths();
+    Array<TextureRegion> frames = new Array<>();
+    for (int i = 0; i < paths.length; i++) {
+      Texture texture = new Texture(Gdx.files.internal(paths[i]));
+      TextureRegion region = new TextureRegion(texture);
+      frames.add(new TextureRegion(region));
+    }
+    return new Animation<TextureRegion>(0.3f, frames, Animation.PlayMode.LOOP);
+  }
 }
