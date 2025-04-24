33fca542b946932b0ce7f8b42b22b5c835c4002f
gufla5085 <gufla5085@uib.no>
2025-04-24 10:21:36 +0200

    Renamed ButtonFactory to ButtonDesigner

--- src/main/java/inf112/firegirlwaterboy/view/ButtonDesigner.java
+++ src/main/java/inf112/firegirlwaterboy/view/ButtonDesigner.java
@@ -14,7 +14,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 /**
  * ButtonFactory creates reusable buttons with consistent styling.
  */
-public class ButtonFactory {
+public class ButtonDesigner {
 
     /**
      * Creates a TextButton with custom text and fill color.
