c917c5ea3341f310512fa1bb898ff70587d91e86
henrikpopos <frishcoco@gmail.com>
2025-04-30 14:57:44 +0200

    add TODO in test
    Either delete or fix this.

--- src/test/java/inf112/firegirlwaterboy/model/ModelTest.java
+++ src/test/java/inf112/firegirlwaterboy/model/ModelTest.java
@@ -148,12 +148,13 @@ public class ModelTest {
 
     @Test
     void testRestartGame() {
+        // TODO FIX ME
         // TiledMap mockMap = mock(TiledMap.class);
         // when(mockManager.getMap("map1")).thenReturn(mockMap);
         // testModel.restartGame();
         // verify(mockFactory).createCollectables(mockWorld, mockMap);
         // verify(mockFactory).createPlatforms(mockWorld, mockMap);
-        // verify(mockFactory).createElements(mockWorld, mockMap);       
+        // verify(mockFactory).createElements(mockWorld, mockMap);
     }
 
     @Test
